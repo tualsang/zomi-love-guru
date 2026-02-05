9e38cb86454d5bc44d871ac508095f3492090114
Tuan Sian Sang <tuansiansang@MacBook-Air.bsp>
2026-02-05 13:56:39 -0500

    disable zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 5,
-  userScalable: true,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: '#ec4899',
 };
 
